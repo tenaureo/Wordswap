a6bac4f4d3fd1f79d950b7fd4fd597d744e8f1c3
Tenzin Thobten Leduk <50613123+tenaureo@users.noreply.github.com>
2020-07-07 19:40:44 +0200

    Update Wordswap.py

--- Wordswap.py
+++ Wordswap.py
@@ -5,15 +5,17 @@ filepath = input("Please enter your filepath: ")
 
 wordToReplace = input("Word to replace: ")
 
+#File is opened in read mode
 f = open(filepath, "r+")
 data = f.read()
 
 wordToReplaceWith = input("Word to replace with: ")
 
-#Word to replace is replaced here
+#The words which will be replaced in the textfile are saved into a variable, the file is closed
 data = data.replace(wordToReplace, wordToReplaceWith)
 f.close
 
+#The file is opened in write mode, the variable is written into the file, the file is closed
 f = open(filepath, "w+")
 f.write(data)
 f.close
